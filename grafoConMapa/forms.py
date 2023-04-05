5297361eb2f41c722937be4d07a2bbb342a5f8a2
Cristhian Agamez <93165832+CristhianAC@users.noreply.github.com>
2023-04-05 13:06:23 -0500

    Web interactiva estetica

--- grafoConMapa/forms.py
+++ grafoConMapa/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class addCity(forms.Form):
-    City= forms.CharField(label="Ingrese la ciudad de origen", max_length=200, widget=forms.TextInput(attrs={"background-color":"black"}))
+    City= forms.CharField(label="Ingrese la ciudad de origen", max_length=200, widget=forms.TextInput(attrs={"color":"black"}))
+    destiny = forms.CharField(label="Ingrese la ciudad del destino",max_length=200, widget=forms.TextInput(attrs={"color":"black"}), required=False)
