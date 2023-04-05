eba6aaf7874021ac2b2acb576b0261056e3f38f9
Cristhian Agamez <93165832+CristhianAC@users.noreply.github.com>
2023-04-04 21:04:39 -0500

    MapaDjango

--- grafoConMapa/views.py
+++ grafoConMapa/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import folium
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+def mapa(request):
+    m = folium.Map()
+    m = m._repr_html_()
+    context = {
+        'm' : m,
+    }
+    return render(request, 'sistema.html',context)
